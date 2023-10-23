1119bff7b540a219c6ce6278993a2de4560216b8
Your Name <fdfdfss@fedsf.com>
2023-10-23 18:53:08 +0300

    second commit

--- src/Lottery.java
+++ src/Lottery.java
@@ -3,12 +3,23 @@ import  java.util.Arrays;
 
 public class Lottery {
     public static void main(String[] args) {
-
-//        Random random = new Random();
-//        int[] array = new int[7];
-//        for (int j = 0; j <array.length; j++) {
-//            array[j] = random.nextInt(10);
-//        }
-//        System.out.println(array);
+        Random random = new Random();
+        int[] array = new int[7];
+        int[] array2 = new int[7];
+        for (int j = 0; j <array.length; j++) {
+            array[j] = random.nextInt(10);
+            array2[j] = random.nextInt(10);
+        }
+        Arrays.sort(array);
+        Arrays.sort(array2);
+        int sum = 0;
+        for (int j = 0; j <7 ; j++) {
+            if (array[j] == array2[j]){
+                sum++;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array2));
+        System.out.println("Количество совпадений: " + sum);
     }
 }
