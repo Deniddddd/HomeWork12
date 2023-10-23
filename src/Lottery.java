afa9f1bd097800db2e229c021d847aa5a40a7734
Your Name <fdfdfss@fedsf.com>
2023-10-23 18:39:04 +0300

    fisrt commit

---
+++ src/Lottery.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import  java.util.Arrays;
+
+public class Lottery {
+    public static void main(String[] args) {
+
+//        Random random = new Random();
+//        int[] array = new int[7];
+//        for (int j = 0; j <array.length; j++) {
+//            array[j] = random.nextInt(10);
+//        }
+//        System.out.println(array);
+    }
+}
